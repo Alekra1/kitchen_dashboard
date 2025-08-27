5a44edde880415d64f6a06bcfa0718e72f76587f
alekra1 <alekseyuwww12@gmail.com>
2025-08-27 14:42:52 +0300

    3 ready API endpoints: 1. Get all orders 2. Create new order 3. Get order by ID

--- main.go
+++ main.go
@@ -23,9 +23,38 @@ func getOrders(c *gin.Context) {
 	c.IndentedJSON(http.StatusOK, orders)
 }
 
+func postOrders(c *gin.Context) {
+	var newOrder order
+
+	// Call BindJSON to bind the received JSON to
+	// newOrder.
+	if err := c.BindJSON(&newOrder); err != nil {
+		return
+	}
+
+	// Add the new order to the slice.
+	orders = append(orders, newOrder)
+	c.IndentedJSON(http.StatusCreated, newOrder)
+}
+
+func getOrderByID(c *gin.Context) {
+	id := c.Param("id")
+
+	// Loop over the list of orders, looking for
+	// an order whose ID value matches the parameter.
+	for _, a := range orders {
+		if a.ID == id {
+			c.IndentedJSON(http.StatusOK, a)
+			return
+		}
+	}
+	c.IndentedJSON(http.StatusNotFound, gin.H{"message": "order not found"})
+}
+
 func main() {
 	router := gin.Default()
 	router.GET("/orders", getOrders)
-
+	router.POST("/orders", postOrders)
+	router.GET("/orders/:id", getOrderByID)
 	router.Run("localhost:8080")
 }
